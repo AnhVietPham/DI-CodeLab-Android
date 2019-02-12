f11b3c10af39ed6eac6c69f7292677290a34376c
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-02-12 07:03:16 +0700

    Add Dependencies involved in MainActivity.kt

--- app/src/main/java/com/example/admin/simpleproject/MainActivity.kt
+++ app/src/main/java/com/example/admin/simpleproject/MainActivity.kt
@@ -2,12 +2,83 @@ package com.example.admin.simpleproject
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.example.admin.daggervskoin.R
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import com.example.admin.daggervskoin.R.layout.activity_main
+import com.example.admin.simpleproject.adapter.UserAdapter
+import retrofit2.Retrofit
+import com.example.admin.simpleproject.model.User
+import timber.log.Timber
+import com.google.gson.GsonBuilder
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.OkHttpClient
+import retrofit2.converter.gson.GsonConverterFactory
+import com.example.admin.simpleproject.interfaces.RandomUsersApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var retrofit: Retrofit
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var mAdapter: UserAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activity_main)
+        initViews()
+
+        val gsonBuilder = GsonBuilder()
+        val gson = gsonBuilder.create()
+
+        Timber.plant(Timber.DebugTree())
+
+        val httpLoggingInterceptor =
+            HttpLoggingInterceptor(HttpLoggingInterceptor.Logger { message ->
+                Timber.i(message)
+            })
+        httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val okHttpClient = OkHttpClient()
+            .newBuilder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+
+        retrofit = Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl("https://randomuser.me/")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+
+        populateUsers()
+    }
+
+    private fun populateUsers() {
+        val callUsers = getUserService().getRandomUsers(10)
+        callUsers.enqueue(object : Callback<User>{
+            override fun onFailure(call: Call<User>, t: Throwable) {
+                Timber.i(t.message)
+            }
+
+            override fun onResponse(call: Call<User>, response: Response<User>) {
+                if (response.isSuccessful) {
+                    mAdapter = UserAdapter()
+                    response.body()?.let { user ->
+                        mAdapter.setItems(user.results)
+                    }
+                    recyclerView.adapter = mAdapter
+                }
+            }
+
+        })
+    }
+
+    private fun initViews() {
+        recyclerView = findViewById(com.example.admin.daggervskoin.R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun getUserService() : RandomUsersApi{
+        return retrofit.create(RandomUsersApi::class.java)
     }
 }
--- app/src/main/java/com/example/admin/simpleproject/interfaces/RandomUsersApi.kt
+++ app/src/main/java/com/example/admin/simpleproject/interfaces/RandomUsersApi.kt
@@ -1,4 +1,12 @@
 package com.example.admin.simpleproject.interfaces
 
+import com.example.admin.simpleproject.model.User
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
 interface RandomUsersApi {
+    @GET("api")
+    fun getRandomUsers(@Query("results") size: Int): Call<User>
 }
