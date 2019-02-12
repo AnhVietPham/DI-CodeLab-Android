441d54d577ed95f4fabf08c14d2cdb2c1ea3606c
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-02-12 22:59:51 +0700

    create component + module and Connecting all Modules

--- app/src/main/java/com/example/admin/simpleproject/component/UserComponent.kt
+++ app/src/main/java/com/example/admin/simpleproject/component/UserComponent.kt
@@ -1,4 +1,11 @@
 package com.example.admin.simpleproject.component
 
+import com.example.admin.simpleproject.interfaces.RandomUsersApi
+import com.squareup.picasso.Picasso
+import dagger.Component
+
+@Component
 interface UserComponent {
+    fun getRandomUserService(): RandomUsersApi
+    fun getPicaso(): Picasso
 }
--- app/src/main/java/com/example/admin/simpleproject/module/OkHttpClientModule.kt
+++ app/src/main/java/com/example/admin/simpleproject/module/OkHttpClientModule.kt
@@ -1,4 +1,43 @@
 package com.example.admin.simpleproject.module
 
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import timber.log.Timber
+import java.io.File
+
+
+@Module(includes = [ContextModule::class])
 class OkHttpClientModule {
+    @Provides
+    fun okHttpClient(cache: Cache, httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient()
+            .newBuilder()
+            .cache(cache)
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    fun cache(cacheFile: File): Cache {
+        return Cache(cacheFile, 10 * 1000 * 1000) //10 MB
+    }
+
+    @Provides
+    fun file(context: Context): File {
+        val file = File(context.cacheDir, "HttpCache")
+        file.mkdirs()
+        return file
+    }
+
+    @Provides
+    fun httpLoggingInterceptor(): HttpLoggingInterceptor {
+        val httpLoggingInterceptor =
+            HttpLoggingInterceptor(HttpLoggingInterceptor.Logger { message -> Timber.d(message) })
+        httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        return httpLoggingInterceptor
+    }
 }
